300f310b65b18e09d62951d3901bddb076933545
unknown <sharmadheeraj9341@gmail.com>
2024-06-07 22:02:38 +0530

    instace_update

--- src/Utils/axiosInstance.js
+++ src/Utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://seven-updown-backend-1.onrender.com",
+  baseURL: "https://seven-updown-backend-1.onrender.com/api",
 });
 
 axiosInstance.interceptors.request.use(
